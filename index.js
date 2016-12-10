88d095b1f23cacda20824a08a6b78d6e85e6479b
Salveson <Andrew.Salveson@perkinswill.com>
2016-12-09 22:06:30 -0500

    ya ok

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var MergeUtility = require('./utilities/merge_data');
 
 mergeData = new MergeUtility({
-  key: 'Tax Parcel',
-  // key: 'PARCEL_ID',
+  // key: 'Tax Parcel',
+  key: 'PARCEL_ID',
   // key: 'Reported',
   ignore: [
     'Not Available'
   ],
-  dir: './resources'
+  dir: './resources',
+  out: './utilities/output'
 })
 
 mergeData.loadData( function(){
