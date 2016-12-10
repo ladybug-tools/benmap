482ec506900c0ea5c1d6de06f90314771a18c25b
Salveson <Andrew.Salveson@perkinswill.com>
2016-12-10 01:43:06 -0500

    code reorganization

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,63 +1,99 @@
+
+//
+// page state
+
 var state = {
   filter: "school",
   metric: "Source EUI (kBTU/sf)"
 };
 
-var style       = {};
-var data        = {};
-var olMap       = {};
-var vectorLayer = {};
 
-function trackMetric(metric) {
-    // console.log("Metric: ", metric.value);
-    if (metric.value === "energy") {
-        // redirect to function to recalculate data using EUI column in csv data
-        // trigger setStyles
-    }
-    if (metric.value === "carbon"){
-        // use Carbon Intensity column
-    }
+//
+// objects/variables that need to be accessed within functions
+
+var style        = {};
+var table        = {};
+var olMap        = {};
+var vectorLayer  = {};
+var vectorSource = {};
+var key          = 'PARCEL_ID';
+
+
+//
+// we put the FUN in functions
+
+var updateLayers = function(){
+  vectorLayer.setStyle( setStyles );
 };
 
-$(document).on('change','#metric',function(e){
-  state.metric = $(e.target).val();
-  vectorLayer.redraw();
-});
-
-$(document).on('change','#filter',function(e){
-  state.filter = $(e.target).val();
-  vectorLayer.redraw();
-});
-
-var getFilter = function(filter) {
-    console.log("Filter: ", filter.value);
-    column = filter.value;
-    switch (filter.value) {
-        case ("School") :
-        // do something
-        break;
-        case ("Residential") :
-        // do something else
-        break;
+var handleData = function(values){
+  
+  var localData = table;
+  
+  // values is an array:
+  // [0] csv  file string
+  // [1] json file string
+  var csvRaw  = values.shift();
+  var jsonRaw = values.shift();
+  
+  var rows = Papa.parse(csvRaw, {header: true} ).data;
+  var json = JSON.parse(jsonRaw);
+  
+  for(var r in rows){
+    var row = rows[r];
+    // console.log(Object.keys(row));
+    if(row[ key ]){
+      // console.log('adding row');
+      localData[row[ key ]] = row;
     }
+  }
+      
+  json.features.map(function(feature){
+    setStyles(feature);
+  })
+  // console.log(style);
+  // debugger;
+
+  vectorSource = new ol.source.Vector({
+    features: (new ol.format.GeoJSON()).readFeatures(json)
+  });
+
+  vectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+    style: styleFunction,
+    projection: 'EPSG:4326'
+  });
+  // debugger;
+
+  olMap.addLayer(vectorLayer);
 };
 
 var setStyles = function(feature) {
-    // console.log(feature);
-    var scale = chroma.scale(['white', 'rgba(183,28,28 ,1)']).domain([0, 35]);
     
-    var id = feature.properties.PARCEL_ID;
+    // get feature ID
+    var id;
+    if(feature.properties){
+      id = feature.properties.PARCEL_ID;
+    }else if(feature.T){
+      id = feature.T.PARCEL_ID;
+    }
     
+    // get row from table table by ID
     var row = {};
-    row[ state.metric ] = 0;
-    if(data[id]){
-      row = data[id];
+    if(table[id]){
+      row = table[id];
     }else{
       // console.log('row does not exist');
+      row[ state.metric ] = 0;
     }
+    
+    // logic here for filter (set alpha to 0 if not pass filter?)
 
+    // get value from row
     var value = row[ state.metric ];
 
+    // build color
+    var scale = chroma.scale(['white', 'rgba(183,28,28 ,1)']).domain([0, 35]);
     var color = scale(value).rgb();
     // console.log(value, color);
     // var alpha = scale(value).alpha();
@@ -68,7 +104,7 @@ var setStyles = function(feature) {
 
     // console.log(rgbaStroke);
 
-    return style[feature.properties["PARCEL_ID"]] = new ol.style.Style({
+    return style[ id ] = new ol.style.Style({
         stroke: new ol.style.Stroke({
             color: rgbaStroke,
             width: 2
@@ -78,9 +114,19 @@ var setStyles = function(feature) {
         })
     })
 
+};
+
+var styleFunction = function(feature) {
+    //console.log("in function prop", feature.getProperties());
+    // console.log("in function getkey", feature.getProperties()["PART_USE"]);
+    // var res1 = style[feature.getProperties()["PARCEL_ID"]]
+    // var res = setStyles(feature);
+    // debugger;
+    // console.log(style[feature.getProperties()["PARCEL_ID"]]);
+
+    return style[feature.getProperties()["PARCEL_ID"]];
+    // return style["R2"];
 }
-
-
 
 // var styles = {
 //     'R1': new ol.style.Style({
@@ -112,90 +158,49 @@ var setStyles = function(feature) {
 //     })
 // }
 
-var styleFunction = function(feature) {
-    //console.log("in function prop", feature.getProperties());
-    // console.log("in function getkey", feature.getProperties()["PART_USE"]);
-    // var res1 = style[feature.getProperties()["PARCEL_ID"]]
-    // var res = setStyles(feature);
-    // debugger;
-    // console.log(style[feature.getProperties()["PARCEL_ID"]]);
 
-    return style[feature.getProperties()["PARCEL_ID"]];
-    // return style["R2"];
-}
+//
+// jQuery page listeners
 
+$(document).on('change','#metric',function(e){
+  state.metric = $(e.target).val();
+  updateLayers();
+});
+$(document).on('change','#filter',function(e){
+  state.filter = $(e.target).val();
+  updateLayers();
+});
+
+
+//
+// init on page ready
 
 $(document).ready(function() {
-  
 
-  
-    var readyData = data;
-    olMap = new ol.Map({
-      layers: [
-        new ol.layer.Tile({
-            source: new ol.source.Stamen({
-                layer: "toner-lite"
-            })
-        })
-      ],
-      // overlays: [overlay],
-      target: 'map',
-      controls: ol.control.defaults({
-        attributionOptions: {
-            collapsible: false
-        }
-      }),
-      view: new ol.View({
-        //center: ol.proj.fromLonLat([-71.087955, 42.343583]),
-        center: [-71.087955, 42.343583],
-        zoom: 13,
-        projection: 'EPSG:4326'
+  olMap = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+          source: new ol.source.Stamen({
+              layer: "toner-lite"
+          })
       })
-    });
-  
-  var CSVPromise  = $.get('./data.csv');
-  var JSONPromise = $.getJSON("./geometry.geojson");
-
-  Promise.all([CSVPromise,JSONPromise]).then(function(values){
-    var csv  = values.shift();
-    var json = values.shift();
-    // console.log(csv);
-    
-    var rows = Papa.parse(csv, {header: true} ).data;
-    
-    
-    
-    for(var r in rows){
-      var row = rows[r];
-      // console.log(Object.keys(row));
-      if(row['PARCEL_ID']){
-        // console.log('adding row');
-        readyData[row['PARCEL_ID']] = row;
-      }else{
-        // console.log('no row');
+    ],
+    // overlays: [overlay],
+    target: 'map',
+    controls: ol.control.defaults({
+      attributionOptions: {
+          collapsible: false
       }
-    }
-    
-    // console.log(readyData);
-        
-    json.features.map(function(feature){
-        setStyles(feature);
+    }),
+    view: new ol.View({
+      //center: ol.proj.fromLonLat([-71.087955, 42.343583]),
+      center: [-71.087955, 42.343583],
+      zoom: 13,
+      projection: 'EPSG:4326'
     })
-    // console.log(style);
-    // debugger;
-
-    var vectorSource = new ol.source.Vector({
-        features: (new ol.format.GeoJSON()).readFeatures(json)
-    });
-
-    vectorLayer = new ol.layer.Vector({
-        source: vectorSource,
-        style: styleFunction,
-        projection: 'EPSG:4326'
-    });
-    // debugger;
-
-    olMap.addLayer(vectorLayer);
   });
+  
+  // handle data retrieved via ajax
+  Promise.all( ['./data.csv',"./geometry.geojson"].map($.get) ).then( handleData );
 
 })
