868298f9e470480dcd1b6dba251c3aebbf579ca6
Elcin <eertugrul@thorntontomasetti.com>
2016-12-09 19:58:44 -0500

    set style function

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,3 +1,41 @@
+var styles = {
+    'R1': new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: 'magenta',
+            width: 2
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 0, 255, 0.5)'
+        })
+    }),
+    'R2': new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: 'blue',
+            width: 2
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(0, 0, 255, 0.5)'
+        })
+    }),
+    'R3': new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: 'red',
+            width: 2
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(255,0,0,0.5)'
+        })
+    })
+}
+
+var styleFunction = function(feature) {
+    //console.log("in function prop", feature.getProperties());
+    console.log("in function getkey", feature.getProperties()["PART_USE"]);
+
+    return styles[feature.getProperties()["PART_USE"]];
+}
+
+
 $(document).ready(function() {
 
     var olMap = new ol.Map({
@@ -23,96 +61,18 @@ $(document).ready(function() {
     });
 
 
-    $.getJSON("5_building_sample.geojson", function(json) {
+    $.getJSON("5_building_sample_epsg3857.geojson", function(json) {
         console.log(json);
 
-    //     var geojsonObject = {
-    //    'type': 'FeatureCollection',
-    //    'crs': {
-    //      'type': 'name',
-    //      'properties': {
-    //        'name': 'EPSG:3857'
-    //      }
-    //    },
-    //    'features': [{
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'Point',
-    //        'coordinates': [0, 0]
-    //      }
-    //    }, {
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'LineString',
-    //        'coordinates': [[4e6, -2e6], [8e6, 2e6]]
-    //      }
-    //    }, {
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'LineString',
-    //        'coordinates': [[4e6, 2e6], [8e6, -2e6]]
-    //      }
-    //    }, {
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'Polygon',
-    //        'coordinates': [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]]
-    //      }
-    //    }, {
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'MultiLineString',
-    //        'coordinates': [
-    //          [[-1e6, -7.5e5], [-1e6, 7.5e5]],
-    //          [[1e6, -7.5e5], [1e6, 7.5e5]],
-    //          [[-7.5e5, -1e6], [7.5e5, -1e6]],
-    //          [[-7.5e5, 1e6], [7.5e5, 1e6]]
-    //        ]
-    //      }
-    //    }, {
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'MultiPolygon',
-    //        'coordinates': [
-    //          [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6], [-3e6, 6e6]]],
-    //          [[[-2e6, 6e6], [-2e6, 8e6], [0, 8e6], [0, 6e6]]],
-    //          [[[1e6, 6e6], [1e6, 8e6], [3e6, 8e6], [3e6, 6e6]]]
-    //        ]
-    //      }
-    //    }, {
-    //      'type': 'Feature',
-    //      'geometry': {
-    //        'type': 'GeometryCollection',
-    //        'geometries': [{
-    //          'type': 'LineString',
-    //          'coordinates': [[-5e6, -5e6], [0, -5e6]]
-    //        }, {
-    //          'type': 'Point',
-    //          'coordinates': [4e6, -5e6]
-    //        }, {
-    //          'type': 'Polygon',
-    //          'coordinates': [[[1e6, -6e6], [2e6, -4e6], [3e6, -6e6]]]
-    //        }]
-    //      }
-    //    }]
-    //  };
+
         var vectorSource = new ol.source.Vector({
             features: (new ol.format.GeoJSON()).readFeatures(json)
         });
 
-        var customStyle = new ol.style.Style({
-            stroke: new ol.style.Stroke({
-                color: 'red',
-                width: 2
-            }),
-            fill: new ol.style.Fill({
-                color: 'rgba(255,0,0,0.2)'
-            })
-        })
 
         var vectorLayer = new ol.layer.Vector({
             source: vectorSource,
-            style : customStyle
+            style: styleFunction
         });
 
         olMap.addLayer(vectorLayer);
