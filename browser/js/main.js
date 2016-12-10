c67b6d9820f527227123bb3493d006e6d847edaf
Margaret <mwang27@gmail.com>
2016-12-09 20:50:21 -0500

    beginning to try colors

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,45 +1,58 @@
-function trackMetric(metric){
+function trackMetric(metric) {
     console.log("Metric: ", metric.value);
 };
 
-function getFilter(filter){
+function getFilter(filter) {
     console.log("Filter: ", filter.value);
 };
+
+var scale = chroma.scale(['rgba(250,250,250 ,1)', 'rgba(183,28,28 ,1)']).domain([0, 30]);
 
 var styles = {
     'R1': new ol.style.Style({
         stroke: new ol.style.Stroke({
-            color: 'magenta',
+            color: 'rgba(142,36,170 ,1)',
             width: 2
         }),
         fill: new ol.style.Fill({
-            color: 'rgba(255, 0, 255, 0.5)'
+            color: 'rgba(142,36,170 ,0.6)'
         })
     }),
     'R2': new ol.style.Style({
         stroke: new ol.style.Stroke({
-            color: 'blue',
+            color: 'rgba(0,172,193 ,1)',
             width: 2
         }),
         fill: new ol.style.Fill({
-            color: 'rgba(0, 0, 255, 0.5)'
+            color: 'rgba(0,172,193 ,0.6)'
         })
     }),
     'R3': new ol.style.Style({
         stroke: new ol.style.Stroke({
-            color: 'red',
+            color: 'rgba(255,179,0 ,1)',
             width: 2
         }),
         fill: new ol.style.Fill({
-            color: 'rgba(255,0,0,0.5)'
+            color: 'rgba(255,179,0 ,0.6)'
         })
     })
 }
 
+
 var styleFunction = function(feature) {
     //console.log("in function prop", feature.getProperties());
     console.log("in function getkey", feature.getProperties()["PART_USE"]);
+    var value = 10.3;
 
+    var color = scale(value).rgb();
+    var alpha = scale(value).alpha();
+
+    console.log("color", scale[0],color, alpha);
+
+
+    styles["R1"].fill = new ol.style.Fill({
+        color: scale(value).rgb()
+    })
     return styles[feature.getProperties()["PART_USE"]];
 }
 
